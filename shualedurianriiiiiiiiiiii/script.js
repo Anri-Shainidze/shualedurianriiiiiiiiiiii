6e0df70f8919c18bf32a5a04547ff6f526bbbabd
Anri-Shainidze <anrianu2006@gmail.com>
2025-01-11 23:16:36 +0400

    Add files via upload

---
+++ shualedurianriiiiiiiiiiii/script.js
@@ -0,0 +1,16 @@
+
+
+const pirveli = document.querySelector(".fa-solid")
+const meore = document.querySelector(".izipizi")
+
+
+
+
+
+pirveli.addEventListener("click", function () {
+    meore.classList.toggle("isarish")
+})
+pirveli.addEventListener("click", function () {
+    pirveli.classList.toggle("rotata")
+})
+
